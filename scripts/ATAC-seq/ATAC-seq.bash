9cf92fd0ee0ead20bfae7788095d7121cdcdf87d
Lan Xu <168080118+xulan92927@users.noreply.github.com>
2025-09-08 16:49:49 +0100

    Update ATAC-seq.bash

--- scripts/ATAC-seq/ATAC-seq.bash
+++ scripts/ATAC-seq/ATAC-seq.bash
@@ -1,4 +1,4 @@
-#Mapping and calling peaks for ATAC-seq
+#Mapping, calling and counting peaks for ATAC-seq
 
 #packages required: chromap, MACS3, samtools, pigz
 
@@ -25,3 +25,16 @@ rm output.sam
 for i in *.align.bed.gz; do
  macs3 callpeak -t $i -n ${i%%.align.bed.gz}_model05_macs3 -f BEDPE -g $genome_size -q 0.05 -B
 done
+
+# merge reads like 
+cat *narrowPeak  | cut -f 1,2,3 | sort -k1,1 -k2,2n -k3,3n | bedtools merge -i - >  all_peaks.bed
+# then conver to "saf" format:
+awk -F $'\t' 'BEGIN {OFS = FS}{ $2=$2+1; peakid="macs3Peak_"++nr;  print peakid,$1,$2,$3,"."}' all_peaks.bed  > all_peaks.saf
+
+# count reads on peaks for each sample with featureCounts
+featureCounts -p -F SAF -a all_peaks.saf --fracOverlap 0.2 -o output.allpeaks.counts output.bam -T 10 
+
+# make counts matrix
+ cut -f 1,2,3,4 output.allpeaks.counts > a
+ for i in *counts; do cut -f 7 $i > $i.dul; done
+ paste a *dul > all_peaks.counts
